e436ca571dac178f9c60f03edad04a864a553e82
ammediagmbh <react-app>
2024-02-22 16:38:48 +0000

    22. Feb. 2024, 17:38

--- keycloakify-9.4.4/dist/login/Template.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import { type TemplateProps } from "../login/TemplateProps";
-import type { KcContext } from "./kcContext";
-import type { I18n } from "./i18n";
-import "../assets/css/override.css";
-export default function Template(props: TemplateProps<KcContext, I18n>): JSX.Element | null;
