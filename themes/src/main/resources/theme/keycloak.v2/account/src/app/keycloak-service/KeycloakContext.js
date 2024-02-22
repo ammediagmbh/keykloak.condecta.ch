5a33b20288d1e4cac556102d63c795a5ce9f49e0
ammediagmbh <react-app>
2024-02-22 17:06:01 +0100

    uipdate

--- themes/src/main/resources/theme/keycloak.v2/account/src/app/keycloak-service/KeycloakContext.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.KeycloakContext = void 0;
-var React = require("react");
-exports.KeycloakContext = React.createContext(undefined);
